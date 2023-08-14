354a7b7ff2c70196d75fd8263832367c8ebb2f18
Taoufiq Arja <taoufiqarja@hotmail.com>
2023-08-14 13:44:21 -0700

    2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 	/*
-	 * Print the specified string with a quotation mark at the beginning
+	 * The main function is where your program starts running. It's like the first step in a recipe. It does the work you tell it to do, and when it's done, it says if the scrip worked
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
