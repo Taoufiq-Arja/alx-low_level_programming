9f845c4eba97412b4621f47db6e4c770b31741ca
Taoufiq Arja <taoufiqarja@hotmail.com>
2023-08-14 14:08:47 -0700

    2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
-* main - this is a main function 
-* description - entry point of the program 
+/*
+* main-this is a main function 
+* description-entry point of the program
+* return-always 0 
 */
-int main(void)
-{
+int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
