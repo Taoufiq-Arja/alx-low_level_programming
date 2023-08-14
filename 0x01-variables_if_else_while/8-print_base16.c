f5a40aa1560d8970385222e5fd711fe63f175c2e
Taoufiq Arja <taoufiqarja@hotmail.com>
2023-08-14 15:42:08 -0700

    2

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char digit = '0';
+
+while (digit <= '9')
+{
+putchar(digit);
+digit++;
+}
+
+char letter = 'a';
+
+while (letter <= 'f')
+{
+putchar(letter);
+letter++;
+}
+
+putchar('\n');
+
+return (0);
+}
