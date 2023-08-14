2c9c38fff6e735198540ebf0a3ee37472ab61408
Taoufiq Arja <taoufiqarja@hotmail.com>
2023-08-14 14:01:03 -0700

    2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-
-int main(void)
-{
-	/**
+ /*
  * main - Entry point of the program
  *
  * Description: This function prints a specific string
@@ -10,6 +7,8 @@ int main(void)
  *
  * Return: Always 0 (Success)
  */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
