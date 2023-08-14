db53d3f0495bb65d330cf48f6759762f12e7b6c0
Taoufiq Arja <taoufiqarja@hotmail.com>
2023-08-14 15:52:28 -0700

    2

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,13 +7,16 @@
 */
 int main(void)
 {
-char digit = '0';
-
-while (digit <= '9')
-{
-putchar(digit);
-digit++;
-}
+putchar('0' + 0);
+putchar('0' + 1);
+putchar('0' + 2);
+putchar('0' + 3);
+putchar('0' + 4);
+putchar('0' + 5);
+putchar('0' + 6);
+putchar('0' + 7);
+putchar('0' + 8);
+putchar('0' + 9);
 
 putchar('\n');
 
